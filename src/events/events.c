d67fe0234e11726897fa54201856bf90610e4908
veilo <vesa.eilo@hotmail.com>
2022-03-24 15:35:21 +0200

    cleaned events

--- src/events/events.c
+++ src/events/events.c
@@ -6,7 +6,7 @@
 /*   By: veilo <veilo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 15:47:20 by veilo             #+#    #+#             */
-/*   Updated: 2022/03/24 15:29:17 by veilo            ###   ########.fr       */
+/*   Updated: 2022/03/24 15:34:37 by veilo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,9 @@ void rotate_light(t_app *app, SDL_Keycode code) {
   }
   app->light_dir[3] = 1;
 }
-
-void events_handle(t_app *app, SDL_Event *event) {
-  float vec[3] = {0, 0, 0};
-  int move_state[6] = {0, 0, 0, 0, 0, 0};
+void get_move_state(int *move_state) {
   const unsigned char *kb_state = SDL_GetKeyboardState(NULL);
+
   if (kb_state[SDL_SCANCODE_W])
     move_state[4] = -1;
   else
@@ -56,6 +54,13 @@ void events_handle(t_app *app, SDL_Event *event) {
     move_state[1] = -1;
   else
     move_state[1] = 0;
+}
+
+void handle_player_movement(t_app *app) {
+  float vec[3] = {0, 0, 0};
+  int move_state[6] = {0, 0, 0, 0, 0, 0};
+
+  get_move_state(move_state);
   lm_vec3_scale(app->move_vector, 0.0, app->move_vector);
   lm_vec3_scale(app->camera_dir, move_state[4] + move_state[5], vec);
   lm_vec3_add(app->move_vector, vec, app->move_vector);
@@ -63,80 +68,34 @@ void events_handle(t_app *app, SDL_Event *event) {
   lm_vec3_add(app->move_vector, vec, app->move_vector);
   lm_vec3_scale(app->camera_up, move_state[2] + move_state[3], vec);
   lm_vec3_add(app->move_vector, vec, app->move_vector);
-  // lm_vec3_scale(app->camera_dir, move_state[5] + move_state[6],
-  // app->move_vector);
-  // if (event->key.keysym.sym == SDLK_w) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_dir, -1.0, app->move_vector);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_dir, -1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
-  // if (event->key.keysym.sym == SDLK_s) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_dir, 1.0, vec);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_dir, 1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
-  // if (event->key.keysym.sym == SDLK_a) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_right, -1.0, vec);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_right, -1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
-  // if (event->key.keysym.sym == SDLK_d) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_right, 1.0, vec);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_right, 1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
-  // if (event->key.keysym.sym == SDLK_DOWN) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_up, -1.0, vec);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_up, -1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
-  // if (event->key.keysym.sym == SDLK_UP) {
-  //   if (event->type == SDL_KEYDOWN) {
-  //     lm_vec3_scale(app->camera_up, 1.0, vec);
-  //     lm_vec3_add(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  //   if (event->type == SDL_KEYUP) {
-  //     lm_vec3_scale(app->camera_right, 1.0, vec);
-  //     lm_vec3_sub(vec, app->move_vector, app->move_vector);
-  //     return;
-  //   }
-  // }
+}
+
+void handle_mouse_look(t_app *app, SDL_Event *event) {
+  if (event->type == SDL_MOUSEMOTION) {
+    float rotmatx[16];
+    float rotmaty[16];
+    float rotmat[16];
+
+    app->yaw += -event->motion.xrel / (180 * M_PI);
+    app->pitch += -event->motion.yrel / (180 * M_PI);
+    if (app->pitch > (2 * M_PI) / 4)
+      app->pitch = (2 * M_PI) / 4;
+    if (app->pitch < -(2 * M_PI) / 4)
+      app->pitch = -(2 * M_PI) / 4;
+    lm_mat4_create_rotmat(rotmatx, (float[3]){0.0, 1.0, 0.0}, app->yaw);
+    lm_mat4_create_rotmat(rotmaty, (float[3]){1.0, 0.0, 0.0}, app->pitch);
+    lm_mat4_multiply(rotmatx, rotmaty, rotmat);
+    lm_mat4vec4_mul(rotmat, VECTOR_Z, app->camera_dir);
+    lm_mat4vec4_mul(rotmat, VECTOR_Y, app->camera_up);
+    lm_vec3_cross(app->camera_up, app->camera_dir, app->camera_right);
+    lm_vec3_normalize(app->camera_right, app->camera_right);
+  }
+}
+
+void events_handle(t_app *app, SDL_Event *event) {
+  handle_player_movement(app);
+  handle_mouse_look(app, event);
+
   if (event->key.keysym.sym == SDLK_q) {
     if (event->type == SDL_KEYDOWN)
       app->rotating = -1;
@@ -161,39 +120,4 @@ void events_handle(t_app *app, SDL_Event *event) {
        (event->key.keysym.sym == SDLK_j || event->key.keysym.sym == SDLK_k ||
         event->key.keysym.sym == SDLK_l)))
     rotate_light(app, event->key.keysym.sym);
-  if (event->type == SDL_MOUSEMOTION) {
-    app->yaw += -event->motion.xrel / (180 * M_PI);
-    app->pitch += -event->motion.yrel / (180 * M_PI);
-    if (app->pitch > (2 * M_PI) / 4)
-      app->pitch = (2 * M_PI) / 4;
-    if (app->pitch < -(2 * M_PI) / 4)
-      app->pitch = -(2 * M_PI) / 4;
-
-    float rotmatx[16];
-    float rotmaty[16];
-    float rotmat[16];
-
-    lm_mat4_create_rotmat(rotmatx, (float[3]){0.0, 1.0, 0.0}, app->yaw);
-    lm_mat4_create_rotmat(rotmaty, (float[3]){1.0, 0.0, 0.0}, app->pitch);
-    lm_mat4_multiply(rotmatx, rotmaty, rotmat);
-
-    lm_mat4vec4_mul(rotmat, VECTOR_Z, app->camera_dir);
-    lm_mat4vec4_mul(rotmat, VECTOR_Y, app->camera_up);
-    // lm_mat4vec4_mul(rotmat, VECTOR_X, app->camera_right);
-
-    lm_vec3_cross(app->camera_up, app->camera_dir, app->camera_right);
-    lm_vec3_normalize(app->camera_right, app->camera_right);
-    // lm_mat4vec4_mul(rotmat, app->camera_right, app->camera_right);
-
-    // ml_matrix4_rotation_x(ml_rad(app->player.rot_y), rotation_y);
-    // ml_matrix4_rotation_y(ml_rad(app->player.rot_x), rotation_x);
-    // ml_matrix4_mul(rotation_x, rotation_y, rotation);
-    // ml_matrix4_mul_vec3(rotation, (t_vec3){0, 0, Z_DIR},
-    // app->player.forward); ml_matrix4_mul_vec3(rotation, (t_vec3){X_DIR, 0,
-    // 0}, app->player.sideways); ml_matrix4_mul_vec3(rotation, (t_vec3){0,
-    // Y_DIR, 0}, app->player.up); ml_matrix4_inverse(rotation,
-    // app->player.inv_rotation); ml_matrix4_copy(rotation,
-    // app->player.rotation);
-
-  } // check book;
 }
